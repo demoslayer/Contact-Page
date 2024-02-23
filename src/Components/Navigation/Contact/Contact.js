4f2bb9aaa147eca0908bacbfb15ef011c0706707
demoslayer <107707267+demoslayer@users.noreply.github.com>
2024-02-24 00:20:12 +0530

    Add files via upload

---
+++ src/Components/Navigation/Contact/Contact.js
@@ -0,0 +1,12 @@
+import styles from './Contact.module.css'
+
+const Contact=()=>{
+    return (
+        <div className={`{styles.contact_section} container`}>
+            <h1>CONTACT US</h1>
+            <p>Lets contact with us , it will be great if we meet and share our ideas for the future Lets contact with us , 
+                it will be great if we meet and share our Lets contact with us , it will be great if we meet and share our</p>
+        </div>
+    );
+};
+export default Contact;
