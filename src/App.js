ed38fdc768714801d92682ccb59106cfdc0fef6b
demoslayer <107707267+demoslayer@users.noreply.github.com>
2024-02-24 00:27:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Contact from './Components/Navigation/Contact/Contact';
+import ContactForm from './Components/Navigation/ContactForm/ContactForm';
+import Navigation from './Components/Navigation/Navigation';
+
+
+function App() {
+  return (
+   <div>
+    <Navigation/>
+    <Contact/>
+    <ContactForm/>
+      </div> 
+  );
+}
+
+export default App;
